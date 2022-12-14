177a1014bba59911279ac6567a1f0f7193aac427
Coder-Lowic <lowiccc@gmail.com>
2022-12-14 22:33:03 +0800

    fix(import):新增分析类逻辑

---
+++ data-analysis/src/main/java/com/lowic/data/analysis/mapper/AnalysisMapper.java
@@ -0,0 +1,18 @@
+package com.lowic.data.analysis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lowic.data.analysis.entity.BusinessReport;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author lowic
+ * @since 2022-12-11
+ */
+public interface AnalysisMapper {
+
+}
