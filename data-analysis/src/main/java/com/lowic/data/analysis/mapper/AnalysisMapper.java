93811beb94c964270982060f0a810a202a59fd47
Coder-Lowic <lowiccc@gmail.com>
2023-01-09 21:58:32 +0800

    feat(报表):关联查询SbInfo和BuRp

--- data-analysis/src/main/java/com/lowic/data/analysis/mapper/AnalysisMapper.java
+++ data-analysis/src/main/java/com/lowic/data/analysis/mapper/AnalysisMapper.java
@@ -1,6 +1,7 @@
 package com.lowic.data.analysis.mapper;
 
 import com.lowic.data.analysis.export.vo.SpCombineBr;
+import org.apache.poi.ss.formula.functions.T;
 
 import java.util.List;
 
@@ -13,6 +14,18 @@ import java.util.List;
  * @since 2022-12-11
  */
 public interface AnalysisMapper {
+    /**
+     * 关联查询 sp_ad_rp 和 business_report 表
+     *
+     * @return 按天和asin汇总的数据
+     */
     List<SpCombineBr> listSpAdRpContactBuRp();
+
+    /**
+     * 关联查询 sb_info 和 business_report 表
+     *
+     * @return 按天和一级类目汇总的数据
+     */
+    List<T> listSbInfoContactBuRp();
 
 }
