01981aa3bc8b64db650d115566b3a2ec0a35c5b5
Coder-Lowic <lowiccc@gmail.com>
2023-04-04 21:13:26 +0800

    fix(import):重构导入方法

--- data-analysis/src/main/java/com/lowic/data/analysis/mapper/SdAdRpMapper.java
+++ data-analysis/src/main/java/com/lowic/data/analysis/mapper/SdAdRpMapper.java
@@ -1,19 +1,19 @@
 package com.lowic.data.analysis.mapper;
 
-import com.lowic.data.analysis.entity.SdAdRp;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lowic.data.analysis.entity.SdAdRp;
 
 import java.util.List;
 
 /**
  * <p>
- *  Mapper 接口
+ * Mapper 接口
  * </p>
  *
  * @author lowic
  * @since 2022-12-05
  */
-public interface SdAdRpMapper extends BaseMapper<SdAdRp> {
+public interface SdAdRpMapper extends BaseMapper<SdAdRp>, SaveBatchExtensionMapper<SdAdRp> {
 
     /**
      * 批量插入
