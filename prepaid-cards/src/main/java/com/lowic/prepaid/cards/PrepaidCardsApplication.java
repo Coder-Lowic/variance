0dea378f9135a836b811261687f4246c18ff9cb9
Coder-Lowic <lowiccc@gmail.com>
2023-12-09 14:30:57 +0800

    feat(prepaid-cards):init初版

---
+++ prepaid-cards/src/main/java/com/lowic/prepaid/cards/PrepaidCardsApplication.java
@@ -0,0 +1,13 @@
+package com.lowic.prepaid.cards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepaidCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepaidCardsApplication.class, args);
+	}
+
+}
---
+++ prepaid-cards/src/test/java/com/lowic/prepaid/cards/PrepaidCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lowic.prepaid.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepaidCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
