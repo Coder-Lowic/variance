ac7fc32bd8308c8929595c192dc292b10fe8ff4b
Coder-Lowic <lowiccc@gmail.com>
2023-12-23 17:22:45 +0800

    feat(prepaid-cards):init初版

---
+++ prepaid-cards/src/main/java/com/lowic/prepaid/cards/controller/RequestController.java
@@ -0,0 +1,10 @@
+package com.lowic.prepaid.cards.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zxf
+ */
+@RestController
+public class RequestController {
+}
---
+++ prepaid-cards/src/main/java/com/lowic/prepaid/cards/service/CardService.java
@@ -0,0 +1,13 @@
+package com.lowic.prepaid.cards.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zxf
+ */
+@Service
+public class CardService {
+    public void getCardInfo() {
+
+    }
+}
