39b160a54d509c75eec372e822f434f4188984d7
Coder-Lowic <lowiccc@gmail.com>
2026-03-25 14:05:41 +0800

    Fix data-analysis module test configuration and ensure project compiles successfully

--- data-analysis/src/test/java/com/lowic/data/analysis/DataAnalysisApplicationTests.java
+++ data-analysis/src/test/java/com/lowic/data/analysis/DataAnalysisApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 /**
  * @author Lowic
  */
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class DataAnalysisApplicationTests {
 
     @Test
